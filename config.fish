5486e658da1054946e3f0974c539e16778ff791e
Trey Eustache <teustache@hennypenny.com>
2025-02-26 20:43:30 -0500

    Removes machine specific configurations.

--- config.fish
+++ config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
 abbr -a swbr git switch
 abbr -a pac sudo pacman -S
 abbr -a pacup sudo pacman -Syyu
@@ -10,10 +11,3 @@ abbr -a gsw git switch
 abbr -a --set-cursor=% gc git commit -m \"%\"
 abbr -a gai git add -i
 abbr -a gd git diff
-
-export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0 #GWSL
-export PULSE_SERVER=tcp:$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}') #GWSL
-export LIBGL_ALWAYS_INDIRECT=1 #GWSL
-
-# Append some STM Paths
-fish_add_path '/opt/stm32cubeclt/GNU-tools-for-STM32/bin'
